9d3a2b43e4b818f64473a596bb4918cc27b4643a
Endel Dreyer <endel.dreyer@gmail.com>
2012-12-16 17:34:07 -0200

    Add support to simple sprite sheets. Add simple behaviour support.

--- Gemfile
+++ Gemfile
@@ -2,7 +2,6 @@ source :rubygems
 
 group :development do
   gem 'guard', '= 1.5.4'
-  gem 'growl'
-  gem 'rb-fsevent', '~> 0.9.1'
   gem 'guard-shell', '= 0.5.1'
+  gem 'rb-fsevent', '~> 0.9.1'
 end
--- dist/joy.js
+++ dist/joy.js
@@ -16,7 +16,8 @@
     Init: {},
     Render: {},
     Input: {},
-    Context: {}
+    Context: {},
+    debug: false
   };
 
   global.Joy = Joy;
@@ -112,7 +113,16 @@ window.onEnterFrame = (function(){
  * @class Engine
  */
 (function(J) {
+  // TODO: find a better way to reference currentEngine instance.
+  // What will happen when we have two canvas contexts at the same time? (like a mini-map?)
+  var currentEngine = null;
+
   var Engine = function(options) {
+    currentEngine = this;
+
+    // Active actors list
+    this.actors = [];
+
     if (options.canvas2d) {
       this.context = new Joy.Context.Context2d({canvas: options.canvas2d});
     }
@@ -125,7 +135,8 @@ window.onEnterFrame = (function(){
       this.useMarkup();
     }
 
-    return this;
+    // requestAnimationFrame
+    this.onEnterFrame();
   };
 
   Engine.prototype.getHeight = function() {
@@ -136,17 +147,39 @@ window.onEnterFrame = (function(){
     return this.context.canvas.width;
   };
 
+  /**
+   * Add a graphic object into context rendering pipeline
+   * @param {Actor, Renderable} node
+   */
   Engine.prototype.addChild = function(node) {
-    return this.context.addChild(node);
+    var renderable;
+
+    if (node instanceof J.Actor) {
+      this.actors.push(node);
+      renderable = node.graphic;
+    } else {
+      renderable = node;
+    }
+
+    return this.context.addChild(renderable);
   };
 
   Engine.prototype.render = function() {
-    return this.context.render();
+    this.context.render();
   };
 
   Engine.prototype.useMarkup = function() {
     var markup = new J.Markup();
     markup.analyse(this.context);
+  };
+
+  /**
+   * Call window's requestAnimationFrame.
+   * @method onEnterFrame
+   */
+  Engine.prototype.onEnterFrame = function () {
+    currentEngine.render();
+    window.onEnterFrame(currentEngine.onEnterFrame);
   };
 
   J.Engine = Engine;
@@ -221,32 +254,82 @@ Joy.Time = {
 };
 
 /**
+ * @class GameObject
+ */
+(function(J) {
+  var Actor = Class.extend({
+    init: function (options) {
+      this.engine = null;
+      this.x = 0;
+      this.y = 0;
+      this.graphic = options.graphic || null;
+
+      this._intervals = {};
+      this._behaviours = [];
+    },
+
+    /**
+     * @method bind
+     * @param {String} action action name (update, key, keypress, keyrelease)
+     * @param {Function} callback
+     */
+    bind: function(action, func) {
+      var behaviour = J.Behaviour.extend({});
+      behaviour.prototype[action] = func;
+      this.addBehaviour(behaviour);
+    },
+
+    /**
+     * @method addBehaviour
+     * @param {Joy.Behaviour} behaviour behaviour class
+     */
+    addBehaviour: function (b) {
+      var self = this,
+          behaviour = new b();
+      if (behaviour.update && !this._intervals.update) {
+        this.addInterval('update', function() {
+          behaviour.update.apply(self);
+        }, 100);
+      }
+      this._behaviours.push(behaviour);
+    },
+
+    /**
+     * @method addInterval
+     * @param {String} id
+     * @param {Function} callback
+     * @param {Number} interval in milliseconds
+     */
+    addInterval: function (id, callback, interval) {
+      // Prevent lost interval reference to keep running.
+      if (this._intervals[id]) {
+        clearInterval(this._intervals[id]);
+      }
+      this._intervals[id] = setInterval(callback, interval);
+    },
+
+    update: function() {
+      var i=0, totalBehaviours = this._behaviours.length;
+      for (;i<totalBehaviours;++i) {
+        this._behaviours[i].update.apply(this);
+      }
+    }
+  });
+
+  J.Actor = Actor;
+})(Joy);
+
+
+/**
  * @class Behaviour
  */
 (function(J) {
   var Behaviour = Class.extend({
-    init: function() {
-    }
+    init: function() {}
   });
 
   Joy.Behaviour = Behaviour;
 })(Joy);
-
-/**
- * @class GameObject
- */
-(function(J) {
-  var GameObject = function(options) {
-    this.x = 0;
-    this.y = 0;
-    this.graphic = options.graphic || null;
-  };
-
-  GameObject.prototype.addBehaviour = function() {};
-
-  J.GameObject = GameObject;
-})(Joy);
-
 
 /**
  * Analyses HTML tags inside <canvas> tag, and add those childs
@@ -358,25 +441,17 @@ Joy.Time = {
  * @class Context2d
 */
 (function(J) {
-  // TODO: find a better way to reference currentContext instance.
-  // What will happen when we have two canvas contexts at the same time? (like a mini-map?)
-  var currentContext = null;
-
   /**
-   * Initializes the currentContext.
+   * Initializes 2d context
    * @param {Object} options
    * @constructor
    */
   var Context2d = function(options) {
-    currentContext = this;
     this.canvas = options.canvas;
     this.context = this.canvas.getContext('2d');
     this.setSmooth(false);
     this.spriteBuffer = {};
     this.pipeline = [];
-
-    // requestAnimationFrame
-    this.onEnterFrame();
   };
 
   /**
@@ -397,15 +472,12 @@ Joy.Time = {
   };
 
   /**
-   * Add a graphic object to enqueue to rendering pipeline.
-   * @param {GameObject, Renderable} node
+   * Add a graphic object into context rendering pipeline
+   * @param {Renderable} node
    */
   Context2d.prototype.addChild = function(node) {
-    var renderable = (node instanceof J.GameObject) ? node.graphic : node;
-    if (renderable) {
-      renderable.setContext(this.context);
-      this.pipeline.push(renderable);
-    }
+    node.setContext(this.context);
+    this.pipeline.push(node);
   };
 
   Context2d.prototype.removeChild = function() {
@@ -447,15 +519,6 @@ Joy.Time = {
     }
   };
 
-  /**
-   * Call window's requestAnimationFrame.
-   * @method onEnterFrame
-   */
-  Context2d.prototype.onEnterFrame = function () {
-    currentContext.render();
-    window.onEnterFrame(currentContext.onEnterFrame);
-  };
-
   // Exports Context2d module
   J.Context.Context2d = Context2d;
 })(Joy);
@@ -495,26 +558,68 @@ Joy.Time = {
 (function(J) {
   var Sprite = J.Renderable.extend({
     init: function(options) {
+      // Asset
       this.asset = options.asset || new Image();
+
+      // Position / Dimensions
       this.x = options.x || 0;
       this.y = options.y || 0;
+      this.width = options.width;
+      this.height = options.height;
 
-      if (options.url) {
-        this.load(options.url);
+      // flip
+      this.flipX = false;
+      this.flipY = false;
+
+      // Frames
+      this.frames = 1;
+      this.currentFrame = 0;
+
+      if (options.src) {
+        this.load(options.src);
       }
 
       this._super();
     },
 
-    load: function(src, onload) {
+    load: function(src) {
+      var self = this;
+
       if (onload) {
-        this.asset.onload = onload;
+        this.asset.onload = function() { self.onLoad(); };
       }
       this.asset.src = src;
     },
 
+    onLoad: function() {
+      if (!this.width) { this.width = this.asset.width; }
+      if (!this.height) { this.height = this.asset.height; }
+
+      // Check frames
+      if (this.width < this.asset.width) {
+        this.frames = Math.ceil(this.asset.width / this.width);
+      }
+      console.log("Number of frames: " + this.frames);
+    },
+
     render: function() {
-      this.ctx.drawImage(this.asset, this.x, this.y, this.asset.width, this.asset.height);
+      this.ctx.save();
+
+      if (this.flipX === true || this.flipY === true) {
+        // TODO: I'm weird and not working as expected
+        this.ctx.translate((this.flipX - 0) * this.width, (this.flipY - 0) * this.height);
+        this.ctx.scale((this.flipX === true) ? -1 : 1 , (this.flipY === true) ? -1 : 1);
+      }
+
+      this.ctx.drawImage(this.asset, this.width * this.currentFrame, 0, this.width, this.height, this.x, this.y, this.width, this.height);
+
+      // Draw debugging rectangle around sprite
+      if (J.debug) {
+        this.ctx.strokeStyle = "red";
+        this.ctx.strokeRect(this.x, this.y, this.width, this.height);
+      }
+
+      this.ctx.restore();
     }
   });
 
