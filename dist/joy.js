e9d427971021860f052f67140b5af231bfbbe3dc
Endel Dreyer <endel.dreyer@gmail.com>
2013-01-18 01:42:42 -0200

    Start mouse events implementation. Only clicks on HUD supported at the moment.

--- dist/joy.js
+++ dist/joy.js
@@ -4,7 +4,7 @@
  * 
  * @copyright 2012-2013 Endel Dreyer 
  * @license MIT
- * @build 1/15/2013
+ * @build 1/18/2013
  */
 
 (function(global) {
@@ -729,6 +729,16 @@
 
     setContext: function(ctx) {
       this.ctx = ctx;
+    },
+
+    /**
+     * @method setScale
+     * @param {Number} x
+     * @param {Number} y
+     */
+    setScale: function (x, y) {
+      this.scale.x = x;
+      this.scale.y = y;
     },
 
     /**
@@ -1679,7 +1689,6 @@
      */
     init: function (options) {
       this._super(options);
-
       this.id = options.id || Joy.generateUniqueId();
 
       /**
@@ -2049,7 +2058,6 @@
 
   /**
    * Engine context. Start your application from here.
-   *
    * @class Engine
    * @constructor
    */
@@ -2089,6 +2097,12 @@
       this.context.canvas.height = options.height;
     }
 
+    // TODO: Implement on-init engine trigger
+    // Enable mouse events, if module is included
+    if (typeof(J.Mouse) !== "undefined") {
+      J.Mouse.enable(this);
+    }
+
     if (options.markup) {
       this.useMarkup();
     }
@@ -2100,9 +2114,20 @@
     this.__defineGetter__('width', function() {
       return this.context.canvas.width;
     });
+
     this.__defineGetter__('height', function() {
       return this.context.canvas.height;
     });
+
+    /**
+     * @property currentScene
+     * @type {Scene}
+     * @readonly
+     */
+    this.__defineGetter__('currentScene', function() {
+      return this.scenes[this._currentScene];
+    });
+    this._currentScene = null;
 
     // requestAnimationFrame
     if (Joy.debug) {
@@ -2154,7 +2179,10 @@
       scene.viewport.addHud(this._frameRateText);
     }
 
-    this.scenes.push(scene);
+    // TODO: implement scene switcher,
+    // setting the current scene to the last added one
+    // is too dumb
+    this._currentScene = this.scenes.push(scene) - 1;
   };
 
   Engine.prototype.render = function() {
@@ -3444,8 +3472,28 @@
     SINGLEQUOTE:222
   };
 
+  /**
+   * Events.KEY_DOWN
+   * @type {String}
+   * @static
+   * @final
+   */
   J.Events.KEY_DOWN = 'onkeydown';
+
+  /**
+   * Events.KEY_UP
+   * @type {String}
+   * @static
+   * @final
+   */
   J.Events.KEY_UP = 'onkeyup';
+
+  /**
+   * Events.KEY_PRESS
+   * @type {String}
+   * @static
+   * @final
+   */
   J.Events.KEY_PRESS = 'key';
 
   Keyboard.handlers = {};
@@ -3508,13 +3556,116 @@
 })(Joy);
 
 (function(J) {
-  J.Events.CLICK = 'click';
-  J.Events.DOUBLE_CLICK = 'dblclick';
-  J.Events.MOUSE_MOVE = 'mousemove';
+  /**
+   * Events.MOUSE_DOWN
+   * @type {String}
+   * @static
+   * @final
+   */
   J.Events.MOUSE_DOWN = 'mousedown';
+
+  /**
+   * Events.MOUSE_UP
+   * @type {String}
+   * @static
+   * @final
+   */
   J.Events.MOUSE_UP = 'mouseup';
 
-  var Mouse = {};
+  /**
+   * Events.CLICK
+   * @type {String}
+   * @static
+   * @final
+   */
+  J.Events.CLICK = 'click';
+
+  /**
+   * Events.DOUBLE_CLICK
+   * @type {String}
+   * @static
+   * @final
+   */
+  J.Events.DOUBLE_CLICK = 'dblclick';
+
+  /**
+   * Events.MOUSE_MOVE
+   * @type {String}
+   * @static
+   * @final
+   */
+  J.Events.MOUSE_MOVE = 'mousemove';
+
+  /**
+   * Events.MOUSE_OVER
+   * @type {String}
+   * @static
+   * @final
+   */
+  J.Events.MOUSE_OVER = 'mouseover';
+
+  /**
+   * Events.MOUSE_OUT
+   * @type {String}
+   * @static
+   * @final
+   */
+  J.Events.MOUSE_OUT = 'mouseout';
+
+  // Create a 1x1 mouse collider
+  var mouseCollider = new J.RectCollider(new J.Vector2d(), 2, 2);
+
+  var Mouse = {
+    enable: function(engine) {
+      engine.context.canvas.onclick = function(e) {
+        var i, length,
+            handlers = Mouse.handlers[e.type];
+
+        // Update collider position
+        mouseCollider.collidePosition.x = e.offsetX * engine.currentScene.viewport.scale.x;
+        mouseCollider.collidePosition.y = e.offsetY * engine.currentScene.viewport.scale.y;
+        //mouseCollider.updateColliderPosition(engine.currentScene.viewport.position);
+
+        for (i=0, length = handlers.length; i<length; ++i) {
+          console.log(mouseCollider, mouseCollider.collidePosition.toString(), handlers[i].target.collider, handlers[i].target.collidePosition.toString());
+
+          if (handlers[i].target.visible && handlers[i].target.collide(mouseCollider)) {
+            console.log(handlers[i].handler);
+            handlers[i].handler.apply(handlers[i].target, [e]);
+          }
+        }
+      };
+
+      engine.context.canvas.onmousedown = function(e) {
+        console.log("Mouse down!", e);
+      };
+      engine.context.canvas.onmouseup = function(e) {
+        console.log("Mouse up!", e);
+      };
+      engine.context.canvas.onmousemove = function(e) {
+        mouseCollider.collidePosition.x = e.offsetX;
+        mouseCollider.collidePosition.y = e.offsetY;
+        console.log("Mouse move!", e);
+      };
+    },
+
+    handlers: {}
+  };
+
+  [J.Events.CLICK, J.Events.DOUBLE_CLICK, J.Events.MOUSE_MOVE, J.Events.MOUSE_DOWN, J.Events.MOUSE_UP].forEach(function(eventType) {
+    Mouse.handlers[eventType] = [];
+
+    J.Triggerable.register(eventType, function(evt) {
+      if (Mouse.handlers[eventType].indexOf(evt) === -1) {
+        Mouse.handlers[eventType].push(evt);
+      }
+    }, function(evt) {
+      var index = Mouse.handlers[eventType].indexOf(evt);
+      if (index !== -1) {
+        Mouse.handlers[eventType].splice(index, 1);
+      }
+    });
+  });
 
   J.Mouse = Mouse;
 })(Joy);
